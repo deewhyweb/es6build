da157ac59749d304614249ac432d2d89bcd7c06f
Philip Hayes <phayes@redhat.com>
2019-12-19 08:32:51 -0500

    Update

--- www/core/rhmi/rhmi-sync-es6.js
+++ www/core/rhmi/rhmi-sync-es6.js
@@ -1,37 +1,79 @@
+"use strict";
 import {
-    createClient,
-    VoyagerClient,
-    DataSyncConfig,
-    OfflineQueueListener,
-    ConflictListener,
-    AuthContextProvider
-  } from "@aerogear/voyager-client";
-  import gql from "graphql-tag";
-  angular
-    .module('phoenix.core.rhmi.sync.poc', [])
-    .factory('rhmiSync', function() {
-    //$scope.message = "Hello World";
-    const config = {
-      httpUrl: "http://localhost:8001/graphql",
-      wsUrl: "ws://localhost:8001/graphql"
-    };
-  
+  createClient,
+  VoyagerClient,
+  DataSyncConfig,
+  OfflineQueueListener,
+  ConflictListener,
+  AuthContextProvider
+} from "@aerogear/voyager-client";
+import gql from "graphql-tag";
+angular.module("phoenix.core.rhmi.sync", []).factory("rhmiSync", function() {
+  //$scope.message = "Hello World";
+  const config = {
+    httpUrl: "http://localhost:8001/graphql",
+    wsUrl: "ws://localhost:8001/graphql"
+  };
+
+  const TASKS_SUBSCRIPTION = gql`
+  query allTasks {
+    allTasks {
+      projectId
+      projectNumber
+      ebsProjectName
+      projectName
+      lastUpdateDate
+      creationDate
+      startDate
+      endDate
+      branchNo
+      costCenter
+      enabledFlag
+      projectType
+      taskNumber
+      taskID
+      taskName
+      taskStartDate
+      taskEndDate
+      curentDate
+      expenditureTypes
+    }
+  }
+`
+  function getAllTasks(cb) {
     createClient(config).then(client => {
       client
         .query({
           fetchPolicy: "network-only",
-          query: gql`
-            query allTasks {
-              allTasks {
-                name
-              }
-            }
-          `
+          query: TASKS_SUBSCRIPTION
         })
         //Print the response of the query
         .then(({ data }) => {
-            console.log(data);
+          cb(null, data.allTasks);
+        })
+        .catch(err => {
+          cb(err);
         });
     });
-  });
-  
+  }
+  function subscribeToTasks() {
+    createClient(config).then(client => {
+      const tasks = client.watchQuery({
+        fetchPolicy: "network-only",
+        query: TASKS_SUBSCRIPTION
+      });
+
+      tasks.subscribeToMore({
+        document: TASKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          // Update logic here.
+        }
+      });
+      return tasks;
+    });
+  }
+  return {
+    getAllTasks,
+    subscribeToTasks
+  };
+});
